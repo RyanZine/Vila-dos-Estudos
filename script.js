7326bcc8ee882f39de44c8d6da565fecc4f6b8af
RyanZine <ryanzinedine.arvis@gmail.com>
2026-04-01 22:31:58 -0300

    feat: função carrossel e conteúdo de matérias

--- script.js
+++ script.js
@@ -1,75 +1,100 @@
-//carrossel
-
-const slides = [
+const materias = [
     {
-    titulo: "Dia 1 - Linguagens",
-    descricao: "Interpretação de textos, coesão e escrita."
+        nome: "Matemática",
+        aulas: [
+            {
+                titulo:"Porcentagem",
+                descricao: "Aprenda cálculos percentuais básicos.",
+                conteudo: `<p>Porcentagem representa uma razão de 100.</p>
+                <p>Exemplos: 50% = 50/100 = 0.5</p>`
+            },
+            {
+                titulo:"Juros Compostos",
+                descricao: "Entenda crescimento exponencial."
+            }
+        ]
     },
     {
-    titulo: "Dia 1 - Matemática",
-    descricao: "Operações, porcentagem e matemática financeira."
+        nome:"Linguagens",
+        aulas: [
+            {
+                titulo:"Interpretação de Texto",
+                descricao:"Como entender textos corretamente."
+            }
+        ]
     },
     {
-    titulo: "Dia 1 - Humanas",
-    descricao: "História geral: Egito, Grécia, Roma."
-    },
-    {
-    titulo: "Dia 1 - Ciências",
-    descricao: "Química e física: atomos e cinemática."
-    },
-    {
-    titulo: "Dia 2 - Linguagens",
-    descricao: "Leitura Guiada e interpretação."
-    },
-    {
-    titulo: "Dia 2 - Matemática",
-    descricao: "juros simples e compostos."
+        nome:"Ciências",
+        aulas: [
+            {
+                titulo:"Átomos",
+                descricao:"estrutura básica da matéria."
+            }
+        ]
     }
 ];
 
 const slide = document.getElementById("slide");
 
-let index = 0;
+const estado = {
+    materiaIndex: 0,
+    aulaIndex: 0,
+};
 
 function mostrarSlide() {
+    const materia = materias[estado.materiaIndex];
+    const aula = materia.aulas[estado.aulaIndex];
+
     slide.classList.remove("ativo");
+
     setTimeout(() => {
     slide.innerHTML = `
-    <h3>${slides[index].titulo}</h3>
-    <p>${slides[index].descricao}</p>`;
-    
-    slide.classList.add("ativo");
+    <h3>${materia.nome}</h3>
+    <strong>${aula.titulo}</strong>
+    <p>${aula.descricao}</p>
+    `;
 
-    },300);
-
+        slide.classList.add("ativo");
+    }, 300);
 }
 
-mostrarSlide();
-
 function proximo() {
-    index++;
+    const materia = materias[estado.materiaIndex];
 
-    if (index >= slides.length) {
-        index = 0;
+    estado.aulaIndex++;
+
+    if (estado.aulaIndex >= materia.aulas.length) {
+        estado.aulaIndex = 0;
+        estado.materiaIndex++;
+
+    if (estado.materiaIndex >= materias.length) {
+        estado.materiaIndex = 0;
     }
+}
 
     mostrarSlide();
 }
 
 function voltar() {
-    index--;
+   estado.aulaIndex--;
 
-    if (index < 0) {
-        index = slides.length - 1;
+   if(estado.aulaIndex < 0) {
+    estado.materiaIndex--;
+
+    if(estado.materiaIndex < 0) {
+        estado.materiaIndex = materias.length - 1;
     }
 
-    mostrarSlide();
+    const materia = materias[estado.materiaIndex];
+    estado.aulaIndex = materia.aulas.length - 1;
+   }
+
+   mostrarSlide();
 }
 
 let intervalo = setInterval(proximo, 3000);
 
 const container = document.querySelector(".carrossel-container");
-
 
 container.addEventListener("mouseenter", () => {
     clearInterval(intervalo);
@@ -77,4 +102,58 @@ container.addEventListener("mouseenter", () => {
 
 container.addEventListener("mouseleave", () => {
     intervalo = setInterval(proximo, 3000);
-}) 
+});
+
+const btnProximo = document.getElementById("btn-proximo");
+const btnVoltar = document.getElementById("btn-voltar");
+
+btnProximo.addEventListener("click", proximo);
+btnVoltar.addEventListener("click", voltar);
+
+mostrarSlide();
+
+const listaMaterias =
+document.getElementById("lista-materias");
+
+function renderMaterias() {
+    listaMaterias.innerHTML = "";
+
+    materias.forEach((materia, index) => {
+        const card = document.createElement("div");
+
+        card.innerHTML = `
+        <h3>${materia.nome}</h3>
+        <p>${materia.aulas.length} aulas</p>
+        `;
+
+        card.addEventListener ("click", () => {
+            selecionarMateria(index);
+        });
+
+        listaMaterias.appendChild(card);
+    });
+}
+
+function selecionarMateria(index) {
+    estado.materiaIndex = index;
+    estado.aulaIndex = 0;
+
+    mostrarSlide();
+}
+
+renderMaterias();
+
+const tiutloAula =
+document.getElementById("titulo-aula");
+const textoAula =
+document.getElementById("texto-aula");
+
+function mostrarConteudo() {
+    const materia = materias[estado.materiaIndex];
+    const aula = materia.aulas[estado.aulaIndex];
+
+    tiutloAula.innerText = aula.titulo;
+    textoAula.innerHTML = aula.conteudo;
+}
+
+mostrarConteudo();
